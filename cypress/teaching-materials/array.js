901b0b811b2ca795e52150e7e14b9b5ae636e20b
StakeOut92 <inozemtcew@gmail.com>
2022-08-23 19:09:04 +0300

    Add methods according array tasks

---
+++ cypress/teaching-materials/array.js
@@ -0,0 +1,12 @@
+import arrayMethodsPage from "../page-objects/arrayMethodsPage";
+
+describe("Array methods", () => {
+    it("", () => {
+        arrayMethodsPage.taskNumberOne()
+        arrayMethodsPage.taskNumberTwo()
+        arrayMethodsPage.taskNumberThree()
+        arrayMethodsPage.taskNumberFour()
+
+    })
+
+})
